0baaf0e400687fb42c88618613e4d3fee6f0b640
joshua <josh.newbill08@hotmail.com>
2023-05-06 23:56:41 -0500

    get code, change text

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+   // 
+    let paragraph = document.querySelector("#text")
+   let change = paragraph.textContent = "This is really cool!"
+    console.log(change)
+    
+})
+
+//console.log(document.querySelector("#text"))
